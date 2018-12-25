6610d0d872c2eef12b5deefefeb0582c78c21578
amit1007 <a0655216@gmail.com>
2018-12-26 01:02:07 +0530

    Add files via upload

--- add-more-vehicle.component.ts
+++ add-more-vehicle.component.ts
@@ -18,7 +18,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./add-more-vehicle.component.css']
 })
 export class AddMoreVehicleComponent implements OnInit {
-
+	veh_images:any
 	addvehicleForm : FormGroup;
 	submitted = false;
 	is_v_tc : boolean = false;
@@ -49,6 +49,7 @@ export class AddMoreVehicleComponent implements OnInit {
 
   	ngOnInit() {
 
+        this.veh_images = null;
 	 	this.currentuser = localStorage.getItem('constantVariable.currentUser')
 		if(this.currentuser)
 		{
@@ -80,38 +81,15 @@ export class AddMoreVehicleComponent implements OnInit {
 
 
 		this.addvehicleForm = this.formBuilder.group({
-			veh_wheel_type: [''],
-	        veh_model: ['', [ Validators.required]],
-	        veh_type: ['', [ Validators.required]],
-	        veh_num1 : [''],
-	        veh_num2 : [''],
-	        veh_num3 : [''],
-	        veh_num4 : [''],
-	        veh_city: ['', [ Validators.required]],
-	        veh_capacity: ['', [ Validators.required]],
-	        veh_dimension: ['', [ Validators.required]],
-	        veh_work_days_from:['', [ Validators.required]],
-	        veh_work_days_to:['', [ Validators.required]],
-	        veh_work_dt_from:['', [ Validators.required]],
-	        veh_work_dt_to:['', [ Validators.required]],
-	        // veh_images:[''],
-	        veh_images : [''],
-			veh_registration_no:[''],
-
-
-			itemRows: this.formBuilder.array([this.initVehicleItemRows()]),
-			
-
+		    itemRows: this.formBuilder.array([this.initVehicleItemRows()]),
 		    veh_terms_condition:[''],
 		    operator_type:this.formBuilder.control(this.operator_type),
-			veh_op_id : this.formBuilder.control(this.userid),
-			
-			documentFile:new FormControl(File),
+		    veh_op_id : this.formBuilder.control(this.userid),
 		});
 	}
 
   	
-	initVehicleItemRows() : FormGroup{
+	initVehicleItemRows() {
 	    return this.formBuilder.group({
 	        // list all your form controls here, which belongs to your form array
 	        veh_wheel_type: [''],
@@ -130,9 +108,7 @@ export class AddMoreVehicleComponent implements OnInit {
 	        veh_work_dt_to:['', [ Validators.required]],
 	        // veh_images:[''],
 	        veh_images : [''],
-			veh_registration_no:[''],
-			documentFile:File,
-			
+	        veh_registration_no:[''],
 	    });
 	}
 
@@ -158,36 +134,13 @@ export class AddMoreVehicleComponent implements OnInit {
         return this.addvehicleForm.get('itemRows') as FormArray;
     }
 
-    addNewRow(): void  {
-    //     // control refers to your formarray
-         const control = <FormArray>this.addvehicleForm.controls['itemRows'];
-    //     // add new formgroup
-         control.push(this.initVehicleItemRows());
-    // }
+    addNewRow() {
+        // control refers to your formarray
+        const control = <FormArray>this.addvehicleForm.controls['itemRows'];
+        // add new formgroup
+        control.push(this.initVehicleItemRows());
+    }
 
-	if(this.totalfiles.length!=0)
-	// if( this.itemRows.value[0].doc_name != "" && this.itemRows.value[0].doc_description != "" && ((this.lengthCheckToaddMore) === (this.totalfiles.length)) )
-	if(this.itemRows.value[0].veh_wheel_type!= "" 
-			&& this.itemRows.value[0].veh_model!= "" 
-			&& this.itemRows.value[0].veh_type!= "" 
-			&& this.itemRows.value[0].veh_num2!= "" 
-			&& this.itemRows.value[0].veh_num3!= "" 
-			&& this.itemRows.value[0].veh_num4!= "" 
-			&& this.itemRows.value[0].veh_city!= "" 
-			&& this.itemRows.value[0].veh_capacity!= "" 
-			&& this.itemRows.value[0].veh_dimension!= "" 
-			&& this.itemRows.value[0].veh_work_days_from!= "" 
-			&& this.itemRows.value[0].veh_work_dt_from!= "" 
-			&& this.itemRows.value[0].veh_work_dt_to!= "" 
-			&& this.itemRows.value[0].veh_images!= "" 
-			&& this.itemRows.value[0].veh_registration_no!= ""
-			&& ((this.lengthCheckToaddMore) === (this.totalfiles.length)) )
-		{
-    
-    //this.itemRows.insert(0, this.initVehicleItemRows())
-    //this.lengthCheckToaddMore=this.lengthCheckToaddMore+1;
-	}
-  }
     deleteRow(index: number) {
         // control refers to your formarray
         const control = <FormArray>this.addvehicleForm.controls['itemRows'];
@@ -210,56 +163,28 @@ export class AddMoreVehicleComponent implements OnInit {
 		this.is_veh_tc = true;	
 	}
 
-	public fileSelectionEvent(fileInput: any,oldIndex) {
+	public fileSelectionEvent(e: any,oldIndex) {
 
 		console.log("oldIndex is ", oldIndex);
-		console.log('file input',fileInput.target.files[0]);
-		if (fileInput.target.files && fileInput.target.files.length > 0) {
-			for (var i = 0; i < fileInput.target.files.length; i++) { 
+		console.log('file input',e.target.files[0]);
+		if (e.target.files && e.target.files.length > 0) {
+			for (var i = 0; i < e.target.files.length; i++) { 
 				var reader = new FileReader();
-				reader.readAsDataURL(fileInput.target.files[i]); // read file as data url
-				// reader.onload = (event:any) => { // called once readAsDataURL is completed
-				// 	// this.url = event.target.result;
-
-				// 	this.urls.push(event.target.result);
-				// }
-				// // this.totalfiles.push(e.target.files[i]);
-				// this.Files.push(e.target.files[i]);
-				// this.FileName.push(e.target.files[i].name);
-				
-				// //console.log('total files',this.totalfiles);
-				// console.log('total files',this.Files);
-				// console.log('total file name',this.FileName);
-
-				reader.onload = (event:any) => {
+				reader.readAsDataURL(e.target.files[i]); // read file as data url
+				reader.onload = (event:any) => { // called once readAsDataURL is completed
+					// this.url = event.target.result;
 					this.urls.push(event.target.result);
 				}
-				this.Files.push(fileInput.target.files[i]);
-				this.FileName.push(fileInput.target.files[i].name);
-				if(oldIndex==0)
-				{
-					this.totalfiles.unshift((fileInput.target.files[0]))
-					this.totalFileName.unshift(fileInput.target.files[0].name)
-				  }
-				  else
-				  {
-					this.totalfiles[oldIndex]=(fileInput.target.files[0]);
-					this.totalFileName[oldIndex]=fileInput.target.files[0].name
-				  }
-				 
-					reader.readAsDataURL(fileInput.target.files[0]);
-				  }
-				//   if(this.totalfiles.length == 1)
-				//   {
-				// 	this.lengthCheckToaddMore=1;
-				//   }
-			  
-				}
-
+				// this.totalfiles.push(e.target.files[i]);
+				this.Files.push(e.target.files[i]);
+				this.FileName.push(e.target.files[i].name);
+				
+				console.log('total files',this.totalfiles);
+				console.log('total file name',this.FileName);
 			}
-		
+		}
 
-	
+	}
 
 	goBack(){
 		this.router.navigate(['/myprofile']);
